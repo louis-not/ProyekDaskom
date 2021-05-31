3b92c0c5d9c97ea6b39d74b3125c5ce32211f743
louis-not <mayco.louis@gmail.com>
2021-06-01 06:23:00 +0700

    menu edit

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main (){
 
 	int input;
-	char *playlist_name = "myPlaylist_X";
+	char yorno,*playlist_name = "myPlaylist_X";
 	
 	printf("--------------Playlist Maker--------------\n");
 	while (1){
@@ -14,6 +14,7 @@ int main (){
 		scanf("%d", &input) ;
 		switch (input) {
 			case 1:
+				input = 0 ;
 				printf("\n------------------------------------------\n");
 				printf("Available Playlist:\n1. myPlaylist A\n2. myPlaylist B\n3. myPlaylist C\n");
 				printf("\nChoose a playlist:");
@@ -36,22 +37,30 @@ int main (){
 						scanf("%d", &input) ;
 					} 
 				}
-				printf("%s\n", playlist_name) ;
+				printf("%s\n", playlist_name) ; // for checking only
 				printf("\nDISPLAY PLAYLIST\n") ; //replace with function
 				printf("\n------------------------------------------\n") ;
 				printf("1. Edit Playlist\n2. Delete Playlist");
 				printf("\n\nYour option:");
+				input = 0 ;
 				scanf("%d", &input) ;
-				switch (input){
-					case 1:
-						printf("Edit this playlist\n") ;
-						break ;
-					case 2:
-						printf("Are you sure you want to delete this Playlist?\n") ;	
-						break ;
-					default:
-						printf("Wrong input,Please make sure your input is between 0 to 3") ;
+				while (1){
+					if ( input == 1){
+						printf("\n1.Add Song\n2. Remove song\n\nYour option: ") ;
+						input = 0 ;
+						scanf("%d", &input) ;
 						break ;						
+					} else if ( input == 2){
+						printf("Are you sure you want to delete this Playlist? (y/n)\n") ;
+						scanf("%d",&input);
+						scanf("%c", &yorno) ;
+							if ( yorno == 'Y' || yorno == 'y'){
+								printf("DELETE MEMORY for %s\n", playlist_name); //replace with function
+								break ;
+							} else if ( yorno == 'N' || yorno == 'n') {
+								break;
+							}
+					} else 	printf("Wrong input,Please make sure your input is between 0 to 3") ;
 				}
 				break ;
 			case 2:
@@ -69,6 +78,5 @@ int main (){
 		}
 	}
 	
-		
 	return 0 ;
 }
